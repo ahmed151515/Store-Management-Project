5a0527bf1dc7f16a163841a22e24e69c7eb4c61b
ahmed151515 <ahmedarafa.200444@gmail.com>
2023-09-04 11:38:11 +0300

    work done

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -2,23 +2,27 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <limits>
 using namespace std;
 // function
-void input_num(double &, string);
-void input_num(int &, string);
+template <class type>
+void input_num(type &x, string msg);
 void get_data(string &, double &, int &);
 void add();
 void print_all();
 void _delete();
 void edit();
+void save();
+void load();
 
 class Product
 {
-    double price;
     string name;
+    double price;
     int q;
 
 public:
+    Product(){};
     Product(string name_, double price_, int q_) : name(name_), price(price_), q(q_)
     {
     }
@@ -37,12 +41,51 @@ public:
         price = price_new;
         q = q_new;
     }
-    void delete_elment()
+    
+    void save(ofstream &file)
     {
-        delete this;
+        file << name << endl
+             << price << endl
+             << q << endl;
+    }
+    void load(ifstream &file)
+    {
+        file >> name >> price >> q;
     }
 };
 vector<Product *> list_of_Product;
+
+int main()
+{
+    load();
+    int choice = 0;
+    while (choice != 5)
+    {
+        cout << "[1] add\n[2] show\n[3] delete\n[4] edit\n[5] exit\n";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            add();
+            break;
+        case 2:
+            print_all();
+            break;
+        case 3:
+            _delete();
+            break;
+        case 4:
+            edit();
+            break;
+
+        default:
+            cout << (choice != 5 ? "wrong choice" : "");
+            break;
+        }
+    }
+    save();
+    return 0;
+}
 void get_data(string &name, double &price, int &q)
 {
     cin.ignore();
@@ -89,9 +132,9 @@ void _delete()
 
     int index;
 
-    input_num(index, "choice number of product");
+    input_num(index, "choice number of product: ");
 
-    list_of_Product[index]->delete_elment();
+    delete list_of_Product[index];
     list_of_Product.erase(list_of_Product.begin() + index);
 }
 void edit()
@@ -106,7 +149,7 @@ void edit()
 
     int index;
 
-    input_num(index, "choice number of product");
+    input_num(index, "choice number of product: ");
 
     string name;
     double price;
@@ -116,70 +159,42 @@ void edit()
 
     list_of_Product[index]->edit_elment(name, price, q);
 }
-int main()
+template <class type>
+void input_num(type &x, string msg)
 {
-    int choice = 0;
-    while (choice != 5)
-    {
-        cout << "[1] add\n[2] show\n[3] delete\n[4] edit\n[5] exit\n";
-        cin >> choice;
-        switch (choice)
-        {
-        case 1:
-            add();
-            break;
-        case 2:
-            print_all();
-            break;
-        case 3:
-            _delete();
-            break;
-        case 4:
-            edit();
-            break;
+    cout << msg;
 
-        default:
-            cout << (choice != 5 ? "wrong choice" : "");
-            break;
-        }
-    }
-    return 0;
-}
-void input_num(double &y, string msg = "")
-{
-    try
-    {
-        cout << msg;
-        cin >> y;
-        if (cin.fail())
-        {
-            throw exception();
-        }
-    }
-    catch (exception e)
+    while (!(cin >> x))
     {
         cerr << "wrong value | number only\n";
         cin.clear();
-        cin.ignore(10000, '\n');
-        input_num(y);
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << msg;
     }
 }
-void input_num(int &x, string msg = "")
+
+void save()
 {
-    try
+    ofstream file;
+    file.open("data.txt");
+    for (auto prodect : list_of_Product)
     {
-        cout << msg;
-        cin >> x;
-        if (cin.fail())
-        {
-            throw exception();
-        }
+        prodect->save(file);
     }
-    catch (exception e)
+    file.close();
+}
+void load()
+{
+    ifstream file;
+    file.open("data.txt");
+    if (!file)
     {
-        cerr << "wrong value | number only\n";
-        cin.clear();
-        cin.ignore(10000, '\n');
-        input_num(x);
+        return;
+    }
+    while (!(file.eof()))
+    {
+        Product *tmp = new Product();
+        tmp->load(file);
+        list_of_Product.push_back(tmp);
     }
 }
